0f095c1c11c9949f4dfe88eb491df40f1c4acca0
nandini <NandiniN@dtechnoland.com>
2025-05-03 02:42:54 +0530

    path setup

--- routes/admin.js
+++ routes/admin.js
@@ -13,21 +13,21 @@ router.post("/loginAdmin", authCtrl.loginAdmin);
 router.post("/registerAdmin", authCtrl.registerAdmin);
 
 // localhost:3100/admin/saveSubscription
-router.post("/registerAdmin", subscriptionsMasterCtrl.saveSubscription);
+router.post("/saveSubscription", subscriptionsMasterCtrl.saveSubscription);
 
 // localhost:3100/admin/getSubscriptionList
-router.post("/registerAdmin", subscriptionsMasterCtrl.getSubscriptionList);
+router.post("/getSubscriptionList", subscriptionsMasterCtrl.getSubscriptionList);
 
 // localhost:3100/admin/deleteSubscription
-router.post("/registerAdmin", subscriptionsMasterCtrl.deleteSubscription);
+router.post("/deleteSubscription", subscriptionsMasterCtrl.deleteSubscription);
 
 // localhost:3100/admin/saveTopic
-router.post("/registerAdmin", topicsMasterCtrl.saveTopic);
+router.post("/saveTopic", topicsMasterCtrl.saveTopic);
 
 // localhost:3100/admin/getTopicList
-router.post("/registerAdmin", topicsMasterCtrl.getTopicList);
+router.post("/getTopicList", topicsMasterCtrl.getTopicList);
 
 // localhost:3100/admin/deleteTopic
-router.post("/registerAdmin", topicsMasterCtrl.deleteTopic);
+router.post("/deleteTopic", topicsMasterCtrl.deleteTopic);
 
 module.exports = router;
